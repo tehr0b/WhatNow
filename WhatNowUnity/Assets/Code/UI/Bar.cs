1854b5d08ae53508c00f5d736d52f98700aa71a3
Rob Giusti <rockenstein.rock@gmail.com>
2015-01-24 01:12:41 -0800

    Slider bar!

---
+++ WhatNowUnity/Assets/Code/UI/Bar.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bar : MonoBehaviour {
+	
+	[SerializeField]
+	Image underBar;
+
+	[SerializeField]
+	Image overBarRight;
+
+	[SerializeField]
+	Image overBarLeft;
+
+	[SerializeField]
+	float drainRate;
+
+	[SerializeField]
+	float lerpRate;
+
+	[SerializeField]
+	float target;
+
+	[SerializeField]
+	bool setTarget;
+
+	float _fill = 1;
+	public float fill {
+		set {
+			overBarRight.rectTransform.sizeDelta = new Vector2(
+				value * underBar.rectTransform.sizeDelta.x/2,
+				underBar.rectTransform.sizeDelta.y);
+			overBarLeft.rectTransform.sizeDelta = new Vector2(
+				value * underBar.rectTransform.sizeDelta.x/2,
+				underBar.rectTransform.sizeDelta.y);
+			_fill = value;
+		}
+		get {
+			return _fill;
+		}
+	}
+
+	void Update() {
+
+		if (setTarget) {
+			if (fill < target){
+				fill = Mathf.Min(target, fill + lerpRate * Time.deltaTime);
+			} else {
+				fill = Mathf.Max(target, fill - lerpRate * Time.deltaTime);
+			}
+			if (fill == target) setTarget = false;
+		} else { //If not targetted, drain
+			fill = Mathf.Max (0, fill - drainRate * Time.deltaTime);
+		}
+	}
+
+	[ContextMenu("Reset to full")]
+	public void ResetToFull(){
+		SetTarget(1);
+	}
+
+	public void SetTarget(float val){
+		target = val;
+		setTarget = true;
+	}
+
+}
---
+++ WhatNowUnity/Assets/Code/UI/BarSlider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarSlider : MonoBehaviour {
+
+	[SerializeField]
+	Image sliderImage;
+
+	[SerializeField]
+	Image barImage;
+
+	[SerializeField]
+	Bar bar;
+
+	float limit { 
+		get {
+			return barImage.rectTransform.rect.width/2;
+		}
+	}
+
+	public float moveSpeed;
+	public int direction = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(moveSpeed * direction * Time.deltaTime, 0, 0);
+		if (Mathf.Abs (transform.localPosition.x) > limit) {
+			transform.localPosition = new Vector3(
+				Mathf.Clamp(transform.localPosition.x,
+			            -limit,
+			            limit),
+				transform.localPosition.y,
+				transform.localPosition.z);
+			direction *= -1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log(transform.localPosition.x + ", " + limit + ", " + bar.fill);
+			if (Mathf.Abs(transform.localPosition.x) < limit * bar.fill) {
+				Debug.Log("Hit!");
+			} else {
+				Debug.Log("Miss!");
+			}
+		}
+	}
+}
