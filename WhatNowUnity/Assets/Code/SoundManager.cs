ad007b1df3235a6dcef0a595936c56023344bc54
Rob Giusti <rockenstein.rock@gmail.com>
2015-01-25 13:28:39 -0800

    Sound manager

---
+++ WhatNowUnity/Assets/Code/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	[SerializeField]
+	AudioSource audioSource;
+
+	[SerializeField]
+	AudioClip[] allMusics;
+
+	int lastMusicIndex = -1;
+
+	void Start() {
+		StartRandomMusic ();
+	}
+
+	void StartRandomMusic () {
+		int musicIndex;
+		do {
+			musicIndex = Random.Range(0, allMusics.Length);
+		} while (musicIndex == lastMusicIndex);
+
+		lastMusicIndex = musicIndex;
+		audioSource.clip = allMusics [musicIndex];
+		audioSource.Play();
+	}
+}
